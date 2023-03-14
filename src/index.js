79546ad7e1e0f781664bfc830c65e56c3a2edd8c
Maria Gabriela Galindo <gabi_who@live.cm>
2023-03-14 15:53:37 -0300

    Adiciona requisito 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
-//começa projeto
+
+const path = require('path');
+
+const readJsonData = require('./readFS');
+
+const talkerPath = path.resolve(__dirname, 'talker.json');
+
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+
+app.get('/talker', async (_request, response) => {
+  const talkerData = await readJsonData(talkerPath);
+  response.status(200).json(talkerData);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
